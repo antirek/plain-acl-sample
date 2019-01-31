8408770862b700663a7313f311f71dd489507f8c
Sergey Dmitriev <serge.dmitriev@gmail.com>
2019-01-31 16:26:28 +0700

    init

---
+++ index.js
@@ -0,0 +1,89 @@
+// role -> context -> action
+
+const roles = [
+    {
+        role: 'user',
+        description: 'Простой пользователь',
+    },
+    {
+        role: 'manager',
+        description: 'Менеджер',
+    },
+];
+
+const contexts = [
+    {
+        context: 'phone',
+        description: '',
+        actions: [
+            {
+                action: 'delete',
+                description: 'Удалить',
+            },
+            {
+                action: 'edit_title',
+                description: 'Изменить название',
+            }
+        ]
+    },
+    {
+        context: 'data',
+        actions: [
+            {
+                action: 'print',
+                description: 'Print all data anywhere',
+            }
+        ]
+    }
+];
+
+const rules = {
+    user: {
+        phone: ['edit', 'create_not_exist'],   // user can edit & create_not_exist with phone
+    },
+    manager: {
+        phone: ['delete', 'edit'],             // manager can delete & edit with phone
+        data: ['print'],
+    },
+};
+
+
+const Acl = require('plain-acl');
+const acl = new Acl(rules, contexts, roles);
+
+const user = {
+    role: 'manager'
+}
+
+let Table = require('tty-table');
+
+let headers = []
+headers[0] = {value: 'context'};
+headers[1] = {value: 'action'};
+roles.map((role) => {
+    headers.push({
+        value: role.description
+    });
+});
+
+let rows = [];
+contexts.forEach((context) => {
+    //console.log('context', context.context);
+    context.actions.forEach((action) => {
+        let row = [];
+        row[0] = context.context;
+        row[1] = action.action;
+        //console.log('action', action.action);
+        roles.forEach((role) => {
+            //console.log('role', role.role);
+            row.push(acl.can(role.role, context.context, action.action) ? '1' : '0')
+        })
+        rows.push(row);
+    })
+})
+
+if (acl.can(user.role, 'data', 'print')) {
+    console.log('user can print data');
+    let t3 = Table(headers, rows);
+    console.log(t3.render())
+}
